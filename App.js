551db7018ece1902b1426b6d5a36d3fa6713afe9
waleery <walenda1997@gmail.com>
2024-04-02 20:32:39 +0200

    style: add styles to appContainer an inputContainer

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ export default function App() {
                 <TextInput style={styles.textInput} placeholder="Your course goal!" />
                 <Button title="Add Goal" />
             </View>
-            <View>
+            <View style={styles.goalsContainer}>
               <Text>List of goals...</Text>
             </View>
         </View>
@@ -17,17 +17,27 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding:50,
+    paddingTop:50,
+    paddingHorizontal:16,
+    flex:1
   },
   inputCointainer:{
+    flex: 1,
     flexDirection:"row",
-    justifyContent: "space-between"
+    justifyContent: "space-between",
+    alignItems:"center",
+    marginBottomBottom:24,
+    borderBottomWidth:1,
+    borderBottomColor: "#cccccc"
   },
   textInput:{
     borderWidth:1,
     borderColor: "#cccccc",
-    width:"80%",
+    width:"70%",
     marginRight:8,
     padding:8
+  },
+  goalsContainer:{
+    flex:9
   }
 });
