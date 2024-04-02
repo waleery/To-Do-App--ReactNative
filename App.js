7dd537f1782cf99a3f92ebb4e4dac26baa1cdd38
waleery <walenda1997@gmail.com>
2024-04-02 19:10:36 +0200

    style: created style object

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Another piece of text</Text>
-      <Text style={{marginTop:16, borderColor:"red", borderWidth:"2px", padding:16}}>Hello word!</Text>
-      <Button title='Tap me!' />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text style={styles.dummyText}>Another piece of text</Text>
+            <Text
+                style={{
+                    marginTop: 16,
+                    borderColor: "red",
+                    borderWidth: "2px",
+                    padding: 16,
+                }}
+            >
+                Hello word!
+            </Text>
+            <Button title="Tap me!" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    dummyText: {
+        marginTop: 16,
+        padding: 16,
+        borderColor: "red",
+        borderWidth: "2px",
+    },
 });
