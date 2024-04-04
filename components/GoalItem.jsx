e777935001c02aed35ddd41042fc83a281981624
waleery <walenda1997@gmail.com>
2024-04-04 23:54:00 +0200

    style: add trash icon and remove only after click on icon

--- components/GoalItem.jsx
+++ components/GoalItem.jsx
@@ -1,17 +1,25 @@
+import { useState } from "react";
 import { Pressable, StyleSheet, Text, View } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 const GoalItem = ({ itemData, deleteItem }) => {
+    const [pressed, setPressed] = useState(false);
+
     return (
-        <View style={styles.goalItem}>
-            {/* move pressavble down to get better ripple effect */}
+        <View style={pressed ? styles.pressedItem : styles.goalItem}>
+            <Text style={styles.goalText}>
+                {itemData.index + 1}. {itemData.item.text}
+            </Text>
+
             <Pressable
                 android_ripple={{ color: "#dddddd" }}
-                onPress={() => deleteItem(itemData.item.id)}
-                style={({pressed}) =>pressed && styles.pressedItem}
+                onPress={() => {
+                    deleteItem(itemData.item.id);
+                }}
+                onPressIn={() => setPressed(true)} // Ustawia stan na true podczas naciśnięcia
+                onPressOut={() => setPressed(false)} // Ustawia stan na false po zwolnieniu przycisku
             >
-                <Text style={styles.goalText}>
-                    {itemData.index + 1}. {itemData.item.text}
-                </Text>
+                <Icon name="trash-o" size={30} color="#aaaaaa" />
             </Pressable>
         </View>
     );
@@ -23,12 +31,24 @@ const styles = StyleSheet.create({
         //this doesn't work on ios, we need to wrap text in view component
         borderRadius: 6,
         backgroundColor: "#eeeeee",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: 8,
     },
-    pressedItem:{
-        opacity:0.25
+    pressedItem: {
+        margin: 8,
+        //this doesn't work on ios, we need to wrap text in view component
+        borderRadius: 6,
+        backgroundColor: "#FF7F7F",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: 8,
+        opacity: 0.25,
     },
     goalText: {
-        padding: 8,
+        fontSize: 16,
     },
 });
 export default GoalItem;
